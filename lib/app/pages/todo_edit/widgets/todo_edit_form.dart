744c0d499a11029ba6abfc396fd8077fabb6b413
Ivan Ray Altomera <irayspacii@gmail.com>
2019-12-11 16:40:04 +0800

    feat: dynamic todo edit

--- lib/app/pages/todo_edit/widgets/todo_edit_form.dart
+++ lib/app/pages/todo_edit/widgets/todo_edit_form.dart
@@ -11,6 +11,8 @@ class TodoEditForm extends StatefulWidget {
 
 class TodoEditFormState extends State<TodoEditForm> {
   final _key = GlobalKey<FormState>();
+  String _title = '';
+
   @override
   Widget build(BuildContext context) => Form(
     key: _key,
@@ -19,10 +21,21 @@ class TodoEditFormState extends State<TodoEditForm> {
       children: <Widget>[
         TextFormField(
           decoration: InputDecoration(labelText: 'Title'),
+          onChanged: (value) => _title = value,
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Input is required';
+            }
+            return null;
+          },
         ),
         RaisedButton(
           child: Text('Edit'),
-          onPressed: () => widget.onEdit('test'),
+          onPressed: () {
+            if (_key.currentState.validate()) {
+              widget.onEdit(_title);
+            }
+          }
         ),
       ],
     ),
