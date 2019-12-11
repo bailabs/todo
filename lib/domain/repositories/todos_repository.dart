6e74229f569ba22ece886eacb24637817da8863c
Ivan Ray Altomera <irayspacii@gmail.com>
2019-12-11 10:07:28 +0800

    feat: data repository remove todo

--- lib/domain/repositories/todos_repository.dart
+++ lib/domain/repositories/todos_repository.dart
@@ -3,6 +3,7 @@ import '../entities/todo.dart';
 abstract class TodosRepository {
   void addTodo(Todo todo);
   void completeTodo(int id);
+  void removeTodo(int id);
   int get todosLength;
   List<Todo> get allTodos;
 }
