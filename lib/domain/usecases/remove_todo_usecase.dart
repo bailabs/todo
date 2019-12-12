73b0e97d416c9e15ad47dcc8637fbdfd7023b8de
Ivan Ray Altomera <irayspacii@gmail.com>
2019-12-12 16:02:20 +0800

    refactor: from UseCase to CompletableUseCase

--- lib/domain/usecases/remove_todo_usecase.dart
+++ lib/domain/usecases/remove_todo_usecase.dart
@@ -1,15 +1,18 @@
+import 'dart:async';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 import 'package:flutter_clean_todo/domain/repositories/todos_repository.dart';
 import 'package:rxdart/rxdart.dart';
 
-class RemoveTodoUseCase extends UseCase<void, RemoveTodoUseCaseParams> {
+class RemoveTodoUseCase extends CompletableUseCase<RemoveTodoUseCaseParams> {
   final TodosRepository todosRepository;
   RemoveTodoUseCase(this.todosRepository);
 
   @override
-  Future<Observable<void>> buildUseCaseObservable(RemoveTodoUseCaseParams params) {
+  Future<Observable<void>> buildUseCaseObservable(RemoveTodoUseCaseParams params) async {
+    final StreamController<void> controller = StreamController();
     todosRepository.removeTodo(params.id);
-    return null;
+    controller.close();
+    return Observable(controller.stream);
   }
 }
 
