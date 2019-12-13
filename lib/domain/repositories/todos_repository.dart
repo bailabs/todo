45197e0b9a727a5e0d3886d3488ef1a1b7e2ce6d
Ivan Ray Altomera <irayspacii@gmail.com>
2019-12-13 20:34:23 +0800

    refactor: void to dynamic

--- lib/domain/repositories/todos_repository.dart
+++ lib/domain/repositories/todos_repository.dart
@@ -1,10 +1,10 @@
 import '../entities/todo.dart';
 
 abstract class TodosRepository {
-  void addTodo(Todo todo);
-  void editTodo(int id, String title);
-  void completeTodo(int id);
-  void removeTodo(int id);
+  addTodo(Todo todo);
+  editTodo(int id, String title);
+  completeTodo(int id);
+  removeTodo(int id);
   getTodo(int id);
   int get todosLength;
   get allTodos;
